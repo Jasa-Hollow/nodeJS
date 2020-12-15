21005d1acff6c0638da57d7703214339fc5492f5
Jasa-Hollow <74333561+Jasa-Hollow@users.noreply.github.com>
2020-12-14 19:47:07 -0600

    Delete class1.js

--- class1.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200,{"Content-Type": "text/plain"})
-    res.write("Hola me llamaste??");
-    res.end();
-});
-
-server.listen(7777);
